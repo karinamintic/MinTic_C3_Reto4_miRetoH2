536dfe353b1134222d9a8a766bb92d6a99dd7d54
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-10-24 08:42:04 -0500

    Se creo el proyecto
    Se crea el proyecto

---
+++ mireto4/src/main/java/com/retos/mireto4/Mireto4Application.java
@@ -0,0 +1,13 @@
+package com.retos.mireto4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mireto4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mireto4Application.class, args);
+	}
+
+}
